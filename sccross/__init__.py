cc9a68f26bf147d190df3fab2ba5f42c3ad19332
Xiuhui Yang <xiuhuiyang@mail.sdu.edu.cn>
2023-10-01 03:47:23 +0800

    latest

--- sccross/__init__.py
+++ sccross/__init__.py
@@ -1,6 +1,6 @@
 
 """
-    scCross is a dDeep Learning-Based Model for integration, cross-dataset cross-modality generation and matched muti-omics simulation of single-cell multi-omics data. Our model can also maintain in-silico perturbations in cross-modality generation and can use in-silico perturbations to find key genes.
+    scCross is a Deep Learning-Based Model for integration, cross-dataset cross-modality generation and matched muti-omics simulation of single-cell multi-omics data. Our model can also maintain in-silico perturbations in cross-modality generation and can use in-silico perturbations to find key genes.
 
 
 """
